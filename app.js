1cb1df88a844f82f435a5b42c6784a085d2c7975
Molina-Q <quentinmolina1@gmail.com>
2024-07-02 00:26:28 +0200

    added fav and computed properties

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'https://www.google.com',
             showBooks: true,
             books: [
-                { title: 'Name of the Wind', author: 'Patrick Rothfuss', age: 44, img: 'assets/1.jpg' },
-                { title: 'The Way of Kings', author: 'Brandon Sanderson', age: 42, img: 'assets/2.jpg'},
-                { title: 'Good Omens', author: 'Terry Pratchett & Neil Gaiman', age: 41, img: 'assets/3.jpg'},
+                { title: 'Name of the Wind', author: 'Patrick Rothfuss', age: 44, img: 'assets/1.jpg', isFav: true },
+                { title: 'The Way of Kings', author: 'Brandon Sanderson', age: 42, img: 'assets/2.jpg', isFav: false},
+                { title: 'Good Omens', author: 'Terry Pratchett & Neil Gaiman', age: 41, img: 'assets/3.jpg', isFav: true},
             ],
             x: 0,
             y: 0
@@ -25,6 +25,16 @@ const app = Vue.createApp({
         handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav
+        },
+     
+    },
+    computed: {
+        filteredBooks() {
+
+            return this.books.filter((book) => book.isFav);
         }
     }
 });
