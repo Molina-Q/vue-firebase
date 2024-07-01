7e2bec0201bdb0bd4b3ad6e622afac5365c8c3db
Molina-Q <quentinmolina1@gmail.com>
2024-07-01 20:22:30 +0200

    feat: add functionality to toggle book display and update age

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const app = Vue.createApp();
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title
+        },
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+});
 
 app.mount('#app');
